cd06eed7176397c409a502128d5bcb6cd0c0cf38
小久保力輝 <kokub0303@gmail.com>
2023-03-08 15:19:27 +0900

    boardsモデルを作成、usersと紐付け

---
+++ db/migrate/20230308054238_create_boards.rb
@@ -0,0 +1,11 @@
+class CreateBoards < ActiveRecord::Migration[7.0]
+  def change
+    create_table :boards do |t|
+      t.string :name
+      t.text :description
+      t.integer :user_id
+      
+      t.timestamps
+    end
+  end
+end
