882aa45e4d953b54ba48ee61dbc2a743e63ade2f
小久保力輝 <kokub0303@gmail.com>
2023-03-13 15:39:09 +0900

    flashの日本語化＋varidateが表示されない不具合修正

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,8 +1,7 @@
 class Board < ApplicationRecord
+  # ユーザーに所属している
+  belongs_to :user
 
   validates :name, presence: true
   validates :description, presence: true
-
-   # ユーザーに所属している
-   belongs_to :user
 end
