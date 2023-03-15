c18ca3dc3167413d2891bce821faba656e4d1f67
小久保力輝 <kokub0303@gmail.com>
2023-03-16 08:22:49 +0900

    annotateのインストール

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id          :bigint           not null, primary key
+#  description :text
+#  name        :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :integer
+#
 class Board < ApplicationRecord
   # ユーザーに所属している
   belongs_to :user
--- test/models/board_test.rb
+++ test/models/board_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id          :bigint           not null, primary key
+#  description :text
+#  name        :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :integer
+#
 require 'test_helper'
 
 class BoardTest < ActiveSupport::TestCase
