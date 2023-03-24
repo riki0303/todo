0c8d31d8387486321e566ebfcae16bd8bcd61bf7
小久保力輝 <kokub0303@gmail.com>
2023-03-25 08:09:27 +0900

    ActionTextの導入

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,17 +2,17 @@
 #
 # Table name: boards
 #
-#  id          :bigint           not null, primary key
-#  description :text
-#  name        :string
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
-#  user_id     :integer
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
 #
 class Board < ApplicationRecord
   # ユーザーに所属している
   belongs_to :user
   has_many :tasks, dependent: :destroy
+  has_rich_text :description
 
   validates :name, presence: true
   validates :description, presence: true
