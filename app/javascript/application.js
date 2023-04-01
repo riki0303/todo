d429e6b81a584937392af54fae061a007a99005b
小久保力輝 <kokub0303@gmail.com>
2023-04-02 08:27:25 +0900

    rails7のせい？でaxios読み込めない？

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,18 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-import "trix"
-import "@rails/actiontext"
-import jquery from "jquery"
-window.$ = jquery
-import axios from 'axios';
-
-
-
-
-$('.header__title').on('click', () => {
-  axios.get('/')
-      .then((response) => {
-        console.log(response)
-      })
-})
+import "@hotwired/turbo-rails";
+import "controllers";
+import "trix";
+import "@rails/actiontext";
+import jquery from "jquery";
+window.$ = jquery;
--- config/importmap.rb
+++ config/importmap.rb
@@ -8,7 +8,7 @@ pin_all_from 'app/javascript/controllers', under: 'controllers'
 pin 'trix'
 pin '@rails/actiontext', to: 'actiontext.js'
 pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.4/dist/jquery.js"
-pin "axios", to: "https://ga.jspm.io/npm:axios@1.3.4/index.js"
-pin "#lib/adapters/http.js", to: "https://ga.jspm.io/npm:@jspm/core@2.0.1/nodelibs/@empty.js"
-pin "#lib/platform/node/classes/FormData.js", to: "https://ga.jspm.io/npm:@jspm/core@2.0.1/nodelibs/@empty.js"
-pin "#lib/platform/node/index.js", to: "https://ga.jspm.io/npm:@jspm/core@2.0.1/nodelibs/@empty.js"
+# pin "axios", to: "https://ga.jspm.io/npm:axios@1.3.4/index.js"
+# pin "#lib/adapters/http.js", to: "https://ga.jspm.io/npm:@jspm/core@2.0.1/nodelibs/@empty.js"
+# pin "#lib/platform/node/classes/FormData.js", to: "https://ga.jspm.io/npm:@jspm/core@2.0.1/nodelibs/@empty.js"
+# pin "#lib/platform/node/index.js", to: "https://ga.jspm.io/npm:@jspm/core@2.0.1/nodelibs/@empty.js"
