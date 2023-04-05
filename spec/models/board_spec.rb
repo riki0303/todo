6e9d4e287f491ff2ca99b5482b66e6ab4a3033dd
小久保力輝 <kokub0303@gmail.com>
2023-04-05 18:48:01 +0900

    context,beforeを定義

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -1,16 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Board, type: :model do
-  it 'タイトルと内容が入力されていれば、boardを保存できる' do
-    user = User.create!({
-      email: 'test@example.com',
-      password: 'password'
-    })
-    board = user.boards.build({
-      name: 'aaaaaaaaaaaaaaaa',
-      description: 'bbbbbbbbbbbbbbbbbbbbbb'
-    })
+  context 'タイトルと概要が入力されている場合' do
+    before do
+      user = User.create!({
+        email: 'test@example.com',
+        password: 'password'
+      })
+      @board = user.boards.build({
+        name: 'aaaaaaaaaaaaaaaa',
+        description: 'bbbbbbbbbbbbbbbbbbbbbb'
+      })
+    end
 
-    expect(board).to be_valid
+    it 'タイトルと内容が入力されていれば、boardを保存できる' do
+      expect(@board).to be_valid
+    end
   end
 end
