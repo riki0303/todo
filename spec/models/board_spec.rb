3487a46aa5e94a09a0262a4c1e636ad532accc1a
小久保力輝 <kokub0303@gmail.com>
2023-04-05 18:42:02 +0900

    boardモデルのテスト

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Board, type: :model do
-  it 'タイトルと' do
+  it 'タイトルと内容が入力されていれば、boardを保存できる' do
+    user = User.create!({
+      email: 'test@example.com',
+      password: 'password'
+    })
+    board = user.boards.build({
+      name: 'aaaaaaaaaaaaaaaa',
+      description: 'bbbbbbbbbbbbbbbbbbbbbb'
+    })
 
+    expect(board).to be_valid
   end
 end
